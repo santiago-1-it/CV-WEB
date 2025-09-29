5eacf112900ba0a4b77bdf97eb04b6a2773f9e9b
Santiago Ferretti <santiago.ferretti853@gmail.com>
2025-09-28 21:10:31 -0300

    cambios, modo oscuro

---
+++ js/colorToggle.js
@@ -0,0 +1,18 @@
+const lightModeBtn = document.querySelector(".light-mode-btn");
+const darkModeBtn = document.querySelector(".dark-mode-btn");
+
+lightModeBtn.addEventListener("click", setDarkMode);
+darkModeBtn.addEventListener("click", setLightMode);
+
+function setDarkMode(){
+setUserTheme("dark");
+}
+
+function setLightMode(){
+  setUserTheme("light");
+}
+
+function setUserTheme(newTheme){
+  document.documentElement.setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme);
+}
